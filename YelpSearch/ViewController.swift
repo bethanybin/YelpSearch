aac3b939763c42f1cf3ba15c543692004edb5076
Bethany Bin <bethanybin@Bethanys-MacBook-Pro-4.local>
2018-02-13 16:53:01 -0800

    Initial Commit

---
+++ YelpSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpSearch
+//
+//  Created by Bethany Bin on 2/13/18.
+//  Copyright © 2018 Bethany Bin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
